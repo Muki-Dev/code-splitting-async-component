21b94ef82c372921bf4cb974e5ac25acbdaaac51
Muki <mukidev881@gmail.com>
2024-12-17 16:29:52 +0100

    code splitting with asynchrone component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Page1 from './components/Page1'
+import AsynchroneComponent from './components/AsyncComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      route: 'page1',
+      component: null
+    }
+  }
+
+  onRouteChange = (route) => {
+    this.setState({route: route})
+  }
+  
+  render(){
+    if(this.state.route === 'page1'){
+      return <Page1 onRouteChange={ this.onRouteChange } />
+    }else if(this.state.route === 'page2'){
+      const AsyncPage2 = AsynchroneComponent(() => import('./components/Page2'))
+      return <AsyncPage2 onRouteChange={ this.onRouteChange } />
+    }else if(this.state.route === 'page3'){
+      const AsyncPage3 = AsynchroneComponent(() => import('./components/Page3'))
+      return <AsyncPage3 onRouteChange={ this.onRouteChange } />
+    }
+  
+  }
 }
 
 export default App;
